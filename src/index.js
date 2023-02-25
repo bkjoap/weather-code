9c694aff99c9c2f2167430354b1b607043b3b458
bkjoap <briana.kjoa.payne@gmail.com>
2023-02-25 14:25:48 -0600

    Get current Temp for Chicago
    get current temp upon load for Chicago

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 let axiosUrl = 'https://jsonplaceholder.typicode.com/comments/'
 let apiKey = 'dff5c692192605ee5ed7f95b423ae857'
 let apiUrl =
-  'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=dff5c692192605ee5ed7f95b423ae857'
+  'https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=dff5c692192605ee5ed7f95b423ae857'
 
-console.log(apiUrl.data.main.temp)
+//update weather
+function displayTemperature(response) {
+  let tempElement = Math.round(response.data.main.temp)
+  let currentTemp = document.querySelector('#current-temp')
+  currentTemp.innerHTML = `${tempElement}°`
+}
+
+axios.get(apiUrl).then(displayTemperature)
