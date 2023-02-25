b7ff5da462e7fd79b94f21f1ac2f5ce0ffbef000
bkjoap <briana.kjoa.payne@gmail.com>
2023-02-25 14:11:48 -0600

    Finished CSS
    Finished off design, add search bar and buttons

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let axiosUrl = 'https://jsonplaceholder.typicode.com/comments/'
+let apiKey = 'dff5c692192605ee5ed7f95b423ae857'
+let apiUrl =
+  'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=dff5c692192605ee5ed7f95b423ae857'
+
+console.log(apiUrl.data.main.temp)
