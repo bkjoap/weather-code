aebf7f3b5c885d5810c7d5828e0b5ace746e6d2a
bkjoap <briana.kjoa.payne@gmail.com>
2023-02-25 18:21:41 -0600

    Update date and time in javascript

--- src/index.js
+++ src/index.js
@@ -31,3 +31,60 @@ function displayHumidity(response) {
 axios.get(apiUrl).then(displayHumidity)
 
 //update date
+function changeDate() {
+  let now = new Date()
+  let date = now.getDate()
+  now.getDate()
+  now.getDay()
+  now.getMonth()
+  now.getFullYear()
+  let months = [
+    'Jan',
+    'Feb',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+  let month = months[now.getMonth()]
+
+  let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+  let day = days[now.getDay()]
+  let year = now.getFullYear()
+
+  let todayDate = document.querySelector('#date')
+  todayDate.innerHTML = `${day} ${month} ${date}, ${year}`
+}
+changeDate()
+
+//update hours
+function changeHours() {
+  let now = new Date()
+  let date = now.getDate()
+  now.getMinutes()
+  now.getHours()
+  let hours = now.getHours()
+  if (hours > 12) {
+    hours = `${hours - 12}`
+  }
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  if (hours > 12) {
+    hours = `${hours - 12}`
+  }
+  let minutes = now.getMinutes()
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  let currentTime = document.querySelector('#time')
+  currentTime.innerHTML = `${hours}:${minutes}`
+}
+changeHours()
